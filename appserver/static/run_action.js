9baf18dab501aeffc8d73928ece0b3b221a23fc0
Matthias Bissinger <matthiasbissinger01@gmail.com>
2021-08-10 22:31:17 +0200

    Datamodel created

---
+++ appserver/static/run_action.js
@@ -0,0 +1,22 @@
+require([
+            "jquery",
+            "splunkjs/mvc/searchmanager",
+            "splunkjs/mvc/simplexml/ready!"
+          ], function(
+              $,
+              SearchManager
+          ) {
+              var mysearch = new SearchManager({
+                  id: "mysearch",
+                  autostart: "false",
+                  search: "|makeresults| outputlookup myfile" 
+              });
+              $(".button1").on("click", function (){
+                  var ok = confirm("Are you sure?");
+                  if (ok){
+                      alert('attempted restart!');
+                  } //else {
+                  //    alert('user did not click ok!');
+                  //}
+              });
+         });
